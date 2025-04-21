892b799e99124c4c3da89ee893af8ed0f698f341
ch0ngzheng <chongzheng9@gmail.com>
2025-04-21 19:46:21 +0800

    rebuilt css

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "../app/templates/**/*.html", // all HTML templates
+    "../app/static/js/**/*.js",   // if you use JS files with Tailwind classes
+  ],
   theme: {
     extend: {},
   },
